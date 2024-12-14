5dc868d42693a49099b17a4805b459af087d85e1
Varun Sahni <sahnivarun62@gmail.com>
2024-12-14 19:03:35 +0530

    chore: revert to marked parse

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,25 +2,23 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import cloudflare from '@astrojs/cloudflare';
-import markdownIntegration from '@astropub/md';
+// import markdownIntegration from '@astropub/md';
 
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [tailwind(), markdownIntegration()],
+  integrations: [tailwind()],
   adapter: cloudflare({
 
   }),
-  markdown: {
-  },
   vite: {
     define: {
       "process.env.TURSO_DB_URL": JSON.stringify(process.env.TURSO_DB_URL),
       "process.env.TURSO_AUTH_TOKEN": JSON.stringify(process.env.TURSO_AUTH_TOKEN)
     },
     ssr: {
-      noExternal: ['@astropub/md']
+      noExternal: ['@astrojs/markdown-remark']
     }
   }
 });
